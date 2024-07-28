dcc7315e3d62accd984ed6b4c6974af8f4ebf62a
Jinwook Chang <tranquil_morning@icloud.com>
2024-07-28 22:42:58 +0900

    complete lecture 01

--- part1/01_bigram.py
+++ part1/01_bigram.py
@@ -39,8 +39,10 @@ while is_finished:
     row = P[idx, :]
     idx = torch.multinomial(row, 1, replacement=True).item()
     ch = itos[idx]
+
     if ch == "<.>":
         is_finished = False
+        break
     out.append(ch)
 
 print(out)
@@ -70,3 +72,90 @@ print(f"{-log_likelihood/n}")
 # %%
 # Let make it neural 👌👌👌
 
+## Create training set
+
+xs, ys = [], []
+
+for w in words:
+    chs = ["<.>"] + list(w) + ["<.>"]
+    for ch1, ch2 in zip(chs, chs[1:]): 
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        xs.append(ix1)
+        ys.append(ix2)
+
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+num = xs.nelement()
+
+
+# %%
+import torch.nn.functional as F
+
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator = g, requires_grad = True)
+
+# %%
+for i in range(0,100):
+    # forward pass
+    xenc = F.one_hot(xs, num_classes = 27).float()
+    logits = xenc @ W # log-counts
+    counts = logits.exp() # counts which is equivalent to N
+    probs = counts / counts.sum(1, keepdim = True)
+    loss = -probs[torch.arange(num), ys].log().mean() # + 0.01*(W**2).mean() # L2 Regul
+
+    # backward pass
+    W.grad = None # set to gradient as zero
+    loss.backward()
+    print(loss)
+    # update
+    W.data += W.grad * -50
+
+# %%
+# Check two model returns same result
+g = torch.Generator().manual_seed(2147483647)
+for i in range(5):
+    out1 = []
+    out2 = []
+    ix1 = 26
+    ix2 = 26
+    while True:
+        p1 = P[ix1]
+        ix1 = torch.multinomial(p1, num_samples=1, replacement=True, generator=g).item()
+        # xenc = F.one_hot(torch.tensor([ix2]), num_classes = 27).float()
+        # logits = xenc @ W
+        # counts = logits.exp()
+        # p2 = counts / counts.sum(1, keepdim = True)
+        # ix2 = torch.multinomial(p2, num_samples=1, replacement=True, generator=g).item()
+        out1.append(itos[ix1])
+        # out2.append(itos[ix2])
+        if ix1 == 26:
+            break
+        # if ix2 == 26:
+            # break
+
+    print(''.join(out1))
+
+# %%
+g = torch.Generator().manual_seed(2147483647)
+for i in range(5):
+    out1 = []
+    out2 = []
+    ix1 = 26
+    ix2 = 26
+    while True:
+        # p1 = P[ix1]
+        # ix1 = torch.multinomial(p1, num_samples=1, replacement=True, generator=g).item()
+        xenc = F.one_hot(torch.tensor([ix2]), num_classes = 27).float()
+        logits = xenc @ W
+        counts = logits.exp()
+        p2 = counts / counts.sum(1, keepdim = True)
+        ix2 = torch.multinomial(p2, num_samples=1, replacement=True, generator=g).item()
+        # out1.append(itos[ix1])
+        out2.append(itos[ix2])
+        # if ix1 == 26:
+            # break
+        if ix2 == 26:
+            break
+
+    print(''.join(out2))
